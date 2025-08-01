c0b5f53b4eab1d3833c856e773f8b4d80ae63df0
Wallan <32087983+wallanpsantos@users.noreply.github.com>
2025-07-31 23:40:28 -0300

    feat: ajuste KafkaEventPublisher

--- wallet-dataprovider/src/main/java/com/br/walletdataprovider/kafka/KafkaEventPublisher.java
+++ wallet-dataprovider/src/main/java/com/br/walletdataprovider/kafka/KafkaEventPublisher.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -13,7 +12,6 @@ import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
 public class KafkaEventPublisher implements EventPublisher {
 
     @Value("${wallet.events.topic}")
